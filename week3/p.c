dee6c1c81f570a8a44ba1d6689e9b596a9d3d2b2
amigler <amigler@calpoly.edu>
2023-04-17 20:46:08 +0000

    p.c

--- week3/p.c
+++ week3/p.c
@@ -5,8 +5,13 @@ int main(int argc, char *argv[]) {
 
     int i = 357;
     int *p = &i;
+    
+    int i2 = 123;
+    
+    *p = *p + 10;
 
-    ++*p = *p + 10;
+    p = &i2;  // change p to the location of i2
+    
     
     printf("i = %d\n", *p);
     
