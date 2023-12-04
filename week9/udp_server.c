0cba074bdf7065c89b25a0a0d45b63210ddefdbf
amigler <amigler@calpoly.edu>
2023-12-04 17:20:20 +0000

    a6 tests, UDP code

--- week9/udp_server.c
+++ week9/udp_server.c
@@ -1,44 +1,44 @@
 #include <arpa/inet.h>
 #include <getopt.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MESG "Message from server\n"
+#define MESG "Hello from client\n"
 #define MAXLEN 1000
 
 int main(int argc, char *argv[]) {
     int sock_fd;
     struct sockaddr_in sa;
-    socklen_t len;
-    
-    char localaddr[INET_ADDRSTRLEN];
+    struct hostent *hostent;
     char buff[MAXLEN+1];
-    int port = 10000;
-    
+    int len, mlen;
     socklen_t slen;
+    int port = 10000;
+    char *hostname = "localhost";
+    
+    hostent = gethostbyname(hostname);
     
     sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
     
     sa.sin_family = AF_INET;
-    sa.sin_port = htons(port); // use designated port
+    sa.sin_port = htons(port);
+    sa.sin_addr.s_addr = *(uint32_t*) hostent->h_addr_list[0];
+    mlen = strlen(MESG);
+    len = sendto(sock_fd, MESG, mlen, 0, (struct sockaddr *) &sa, sizeof(sa));
     
-    sa.sin_addr.s_addr = htonl(INADDR_ANY); // any local interfaces
-    bind(sock_fd, (struct sockaddr *) &sa, sizeof(sa));  
-    
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
     slen = sizeof(sa);
-    int n = recvfrom(sock_fd, buff, sizeof(buff),0, (struct sockaddr *) &sa, &slen);
-
-    inet_ntop(AF_INET, &sa.sin_addr.s_addr, localaddr, sizeof(localaddr));
     
-    printf("Message from <%s:%d>: ", localaddr, ntohs(sa.sin_port));
-    fflush(stdout);
-    
-    write(STDOUT_FILENO, buff, strlen(MESG));
-    len = sendto(sock_fd, MESG, strlen(MESG), 0, (struct sockaddr *) &sa, sizeof(sa));
+    len = recvfrom(sock_fd, buff, sizeof(buff), 0, (struct sockaddr *) &sa, &slen);
+    write(STDOUT_FILENO, buff, len);
     
     shutdown(sock_fd, SHUT_RDWR);
     
